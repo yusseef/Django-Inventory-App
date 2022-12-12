7af3c79beb021a4ac983d00074ca8911d253fdb2
Yussef raouf <yussef.raouf11@gmail.com>
2022-12-13 00:08:57 +0200

    editing model fields

--- articles/admin.py
+++ articles/admin.py
@@ -3,5 +3,6 @@ from .models import Articles
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content']
+    list_display = ['title', 'content', 'timestamp', 'updated']
+    search_fields = ['title', 'content']
 admin.site.register(Articles, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=500)
+    timestamp = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now_add=True)
+    publish = models.DateField(auto_now= False, auto_now_add=False,
+     default=timezone.now, null=True, blank=True)
 
     def __str__(self):
         return self.title
