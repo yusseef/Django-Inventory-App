7ad29038d2e6f3e07257faacc357c3c1d323bd2e
Yussef raouf <yussef.raouf11@gmail.com>
2022-12-17 05:21:57 +0200

    Edit a quantity field

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from .validators import validate_unit
+from .utils import number_str_to_float
 User = settings.AUTH_USER_MODEL
 # Create your models here.
 
@@ -23,10 +24,22 @@ class RecipeIngredient(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     quantity = models.CharField(max_length=50)
+    quantity_as_float = models.FloatField(blank = True, null=True)
     unit = models.CharField(max_length=500, validators=[validate_unit] )
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        qty = self.quantity
+        qty_float, qty_floatsuccess = number_str_to_float(qty)
+        if qty_floatsuccess:
+            self.quantity_as_float = qty_float 
+        
+        else:
+            self.quantity_as_float = None
+        
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
